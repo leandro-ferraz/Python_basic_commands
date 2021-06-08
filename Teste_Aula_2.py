55e0ebb380293fdbcbf7dba616896690223086d0
leandro-ferraz <85505558+leandro-ferraz@users.noreply.github.com>
2021-06-08 16:10:51 -0300

    Add files via upload

---
+++ Teste_Aula_2.py
@@ -0,0 +1,89 @@
+#Várias condições
+numero = 5
+
+if numero == 1:
+        print('Você ganhou')
+elif numero == 3:
+        print('Você ganhou')
+elif numero == 5:
+        print('Você ganhou')
+else:
+        print('Você ganhou')
+
+#Operadores Lógicos (Juntando condições em uma linha)
+numero = 3
+
+if numero == 1 or numero == 3 or numero == 5:
+        print('Você ganhou')
+else:
+
+
+
+#Condição diferente (!=) / Senão se = elif
+nome = 'leandro'
+
+if nome != 'felipe':
+        print('Cade o felipe?')
+elif nome == 'felipe':
+        print('É o felipe!')
+  
+#Operadores Lógicos (Juntando condições em uma linha)
+nome = 'leandro'
+sobrenome = 'ferraz'
+idade = 24
+
+if nome == 'leandro' and sobrenome == 'ferraz' and idade == 24:
+        print('É o leandro ferraz, 3º Sgt do Exército Brasileiro')
+else:
+        print('É outra pessoa')
+
+
+
+# Laços de repetição
+numero = 1
+for numero in range(10):
+  print(numero)
+
+
+for numero in range(3,9):
+  print(numero)
+
+for numero in range(3, 16, 2):
+  print(numero)
+
+
+for numero in range(1,11):
+  if numero % 2 == 0:
+    print('O número', numero ,'é par')
+  else:
+    print(f'O número {numero} é impar')
+
+
+
+
+# Laços de repetição
+numero = 1
+for numero in range(10):
+  print(numero)
+
+
+for numero in range(3,9):
+  print(numero)
+
+for numero in range(3, 16, 2):
+  print(numero)
+
+
+for numero in range(1,11):
+  if numero % 2 == 0:
+    print('O número', numero ,'é par')
+  else:
+    print(f'O número {numero} é impar')
+
+
+
+
+
+
+
+
